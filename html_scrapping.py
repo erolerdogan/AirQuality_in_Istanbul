fba536f2df023e6552f5105ad208b244a16b89a0
erol.erdogan <erdoganerol9@gmail.com>
2020-08-10 18:08:10 +0300

    Web scrapping process to collect Data

---
+++ html_scrapping.py
@@ -0,0 +1,38 @@
+"""Air Quality in Istanbul between 2013 and 2019"""
+
+import os
+import time
+import requests
+import sys
+import bs4
+import sklearn
+
+# ------------------- Data Collection Process -------------------
+# Retrieving html from specific url
+
+
+def retrieve_html():
+    for year in range(2013, 2020):
+        for month in range(1, 13):
+            if month < 9:
+                url = 'https://en.tutiempo.net/climate/0{}-{}/ws-170600.html'.format(month, year)
+
+            else:
+                url = 'https://en.tutiempo.net/climate/{}-{}/ws-170600.html'.format(month, year)
+
+            data = requests.get(url)
+            data_utf = data.text.encode('utf=8')
+
+            if not os.path.exists("Data/{}".format(year)):
+                os.makedirs(("Data/{}".format(year)))
+
+            with open("Data/{}/{}.html".format(year, month), "wb") as output:
+                output.write(data_utf)
+
+        sys.stdout.flush()
+
+
+if __name__ == "__main__":
+    start = time.process_time()
+    retrieve_html()
+    print("Process Time: ", (time.process_time() - start))

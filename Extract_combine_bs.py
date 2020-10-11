a76ea26324e1dfb6ed4e8167a59b04c31df4b964
Mustafa Erdogan <mustafa.erdogan@telpa.entp>
2020-10-11 15:55:39 +0300

    Pulling data algorithm  has been changed because of older algorithm was pulling the values missingly

--- Extract_combine_bs.py
+++ Extract_combine_bs.py
@@ -1,46 +1,36 @@
 # Using BeautifulSoup to combine tables
 
-import os
 from bs4 import BeautifulSoup
 import pandas as pd
-import numpy as np
+import re
 import time
+import os
+import numpy as np
 
 
 def meta_data(month, year):
-
     with open("html_data/{}/{}.html".format(year, month), "rb") as html_file:
-        soup = BeautifulSoup(html_file, "lxml")
+        soup = BeautifulSoup(html_file, 'html.parser')
 
     table = soup.find("table", {"class": "medias mensuales numspan"})
 
-    # Pulling and creating whole data into the list
     try:
+        # Pulling the title of the columns
+        cols = [col.text for col in table.tr]
 
-        # Pulling and creating rows of the table
-        rows = [row.text for row in table.tr]
+        # Pulling the values on the table
+        style = soup.select_one('.medias').find_previous('style').string
+        mapper = dict(re.findall(r'([^.]+?)::after{content:"(.*?)"', style))
+        for span in soup.find_all('span', class_=lambda cl: cl in mapper):
+            span.replace_with(mapper[span['class'][0]])
 
-        texts = []
-        for table in soup.find_all("table", {"class": "medias mensuales numspan"}):
-            for tbody in table:
-                for tr in tbody:
-                    te = tr.get_text()
-                    texts.append(te)
-
-        numberOf_rows = round(len(texts) / len(rows))
         data = []
-        for i in range(numberOf_rows):
-            x = []
-            for j in range(len(rows)):
-                x.append(texts[j])
-            data.append(x)
-            del texts[:len(rows)]
+        for row in soup.select('.medias tr'):
+            texts = [td.text for td in row.select('td') if td.text.strip()]
+            data.append(texts[:11])
 
-        # Drop the unnecessary rows
-        data.pop(0)
-        data.pop(-1)
-
-        df = pd.DataFrame(data, columns=rows)
+        # Creating Dataframe.
+        df = pd.DataFrame(data[:-2], columns=cols[:11])
         df.set_index("Day", inplace=True)
         df["Date"] = None
         for i in range(len(df)):
@@ -51,12 +41,12 @@ def meta_data(month, year):
         return df.to_csv("data/{}-{}.csv".format(month, year))
 
     except Exception as e:
-        print("{}-{} is not created. And there error is: {} ".format(month, year, e))
+        print("{}-{} is not created. And the error is: {} ".format(month, year, e))
 
 
 def combined_dataframe(month, year):
-
     return pd.read_csv("data/{}-{}.csv".format(month, year))
+
 
 if __name__ == "__main__":
 
@@ -78,6 +68,7 @@ if __name__ == "__main__":
         for month in range(1, 13):
             new_df = pd.concat([new_df, combined_dataframe(month, year)], ignore_index=True)
 
+    # Due to 2020 year has only 9 months. We are in 19-11-2020
     for year in [2020]:
         for month in range(1, 10):
             new_df = pd.concat([new_df, combined_dataframe(month, year)], ignore_index=True)
